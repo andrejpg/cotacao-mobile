c558251ce2dc86d9f28ed1b6303e7cb4b26336bd
andrejpg <andremoraisbrito556@gmail.com>
2025-06-09 19:28:01 -0300

    feat: aplica API ao controller (onCreate)

--- app/src/main/java/com/example/cotacaomobile/MainActivity.kt
+++ app/src/main/java/com/example/cotacaomobile/MainActivity.kt
@@ -5,16 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var convMoedaAPI: ConvMoedaAPI
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://economia.awesomeapi.com.br/json/last/:moedas")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        convMoedaAPI = retrofit.create(ConvMoedaAPI::class.java)
     }
 }
